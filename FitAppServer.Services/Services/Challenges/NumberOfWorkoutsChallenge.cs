d4bc7446f2731088c355ff683231481a77c836c4
Bartosz Drozd <drozdiibard@gmail.com>
2022-12-26 23:49:45 +0000

    Fix number of workouts challenge

--- FitAppServer.Services/Services/Challenges/NumberOfWorkoutsChallenge.cs
+++ FitAppServer.Services/Services/Challenges/NumberOfWorkoutsChallenge.cs
@@ -14,28 +14,32 @@ public class NumberOfWorkoutsChallenge : IChallenge
         _context = context;
     }
 
-    public async Task Check(WorkoutAction action, Workout payload)
+    public async Task Check(WorkoutAction action, Workout workout)
     {
-        if (action is WorkoutAction.Created or WorkoutAction.Deleted)
+        if (action is not WorkoutAction.Created and not WorkoutAction.Deleted)
         {
-            var challenge = await _context.ChallengeEntries.FirstOrDefaultAsync(
-                q => q.UserId == payload.UserId && q.Challenge.Id == GetId() && q.CompletedAt == null);
-
-            if (challenge == null)
-            {
-                return;
-            }
-
-            var additionValue = 1;
-            // Decrease the counter on workout deletion
-            if (action == WorkoutAction.Deleted)
-            {
-                additionValue = -1;
-            }
-
-            await _context.ChallengeEntries.Where(q => q.UserId == payload.UserId && q.Challenge.Id == GetId())
-                .ExecuteUpdateAsync(q => q.SetProperty(c => c.Value, c => c.Value + additionValue));
+            return;
         }
+
+        var challenge = await _context.ChallengeEntries.FirstOrDefaultAsync(
+            q => q.UserId == workout.UserId && q.Challenge.Id == GetId() && q.CompletedAt == null);
+
+        if (challenge == null)
+        {
+            // TODO: Handle missing ChallengeEntry
+            return;
+        }
+
+        var additionValue = 1;
+
+        // Decrease the counter on workout deletion
+        if (action == WorkoutAction.Deleted)
+        {
+            additionValue = -1;
+        }
+
+        await _context.ChallengeEntries.Where(q => q.UserId == workout.UserId && q.Challenge.Id == GetId())
+            .ExecuteUpdateAsync(q => q.SetProperty(c => c.Value, c => c.Value + additionValue));
     }
 
 
