e338f2cc6191d7518eb87b870581acb226e93587
Bartosz Drozd <drozdiibard@gmail.com>
2023-02-05 00:04:59 +0000

    Change how workout is updated/added

--- DataAccess/Entities/Exercise.cs
+++ DataAccess/Entities/Exercise.cs
@@ -7,7 +7,7 @@ public class Exercise
     public int Id { get; set; }
     public ExerciseInfo ExerciseInfo { get; set; } = null!;
     public int ExerciseInfoId { get; set; }
-    public ICollection<Set> Sets { get; set; } = new List<Set>();
+    public IList<Set> Sets { get; set; } = new List<Set>();
     public Workout Workout { get; set; } = null!;
     public int WorkoutId { get; set; }
 }
