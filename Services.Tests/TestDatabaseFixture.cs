b2be84b4ce4c96849afcf6df0b07a18cfb066ce4
Bartosz Drozd <drozdiibard@gmail.com>
2023-02-05 01:37:35 +0000

    Rename projects and add project with tests

---
+++ Services.Tests/TestDatabaseFixture.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FitAppServer.DataAccess;
+using FitAppServer.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Tests;
+
+public class TestDatabaseFixture
+{
+    private const string ConnectionString =
+        @"Server=127.0.0.1;Port=5432;Database=tests;User Id=postgres;Password=root;";
+
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (_databaseInitialized)
+            {
+                return;
+            }
+
+            using (var context = CreateContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.AddRange(
+                    new ExerciseInfo {Id = 1, Name = "Deadlift"},
+                    new ExerciseInfo {Id = 2, Name = "Bench"},
+                    new ExerciseInfo {Id = 3, Name = "Squat"},
+                    new ExerciseInfo {Id = 4, Name = "Ohp"},
+                    new ExerciseInfo {Id = 5, Name = "Pullups"}
+                );
+
+                context.Add(new User
+                {
+                    Id = Constants.USER_ID, Email = Constants.USER_EMAIL, Username = Constants.USER_USERNAME,
+                    Uuid = Constants.USER_UUID
+                });
+
+                context.Add(new Workout
+                {
+                    Id = Constants.WORKOUT_ID,
+                    Date = null,
+                    StartDate = null,
+                    EndDate = null,
+                    UserId = Constants.USER_ID,
+                    Type = WorkoutTypeCode.None
+                });
+
+                context.SaveChanges();
+            }
+
+            _databaseInitialized = true;
+        }
+    }
+
+    public FitAppContext CreateContext() =>
+        new(new DbContextOptionsBuilder<FitAppContext>().UseNpgsql(ConnectionString).Options);
+}
---
+++ Services.Tests/WorkoutsServiceTest.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FitAppServer.DataAccess.Entities;
+using FitAppServer.Services.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Services.Tests;
+
+public class WorkoutsServiceTest : IClassFixture<TestDatabaseFixture>
+{
+    private TestDatabaseFixture Fixture { get; }
+
+    public WorkoutsServiceTest(TestDatabaseFixture fixture)
+    {
+        Fixture = fixture;
+    }
+
+    [Fact]
+    public async void GetByWorkoutId()
+    {
+        await using var context = Fixture.CreateContext();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var workout = await service.GetByWorkoutIdAsync(Constants.WORKOUT_ID);
+
+        Assert.NotNull(workout);
+    }
+
+    [Fact]
+    public async void GetByWorkoutId_NonExistentId()
+    {
+        await using var context = Fixture.CreateContext();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var workout = await service.GetByWorkoutIdAsync(999);
+
+        Assert.Null(workout);
+    }
+
+    [Fact]
+    public async void GetByUserId()
+    {
+        await using var context = Fixture.CreateContext();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var workout = await service.GetByUserIdAsync(Constants.USER_UUID);
+
+        Assert.Single(workout);
+    }
+
+    [Fact]
+    public async void GetByUserId_NonExistentUser()
+    {
+        await using var context = Fixture.CreateContext();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var workout = await service.GetByUserIdAsync("non_existent");
+
+        Assert.Empty(workout);
+    }
+
+    [Fact]
+    public async void DeleteWorkout()
+    {
+        await using var context = Fixture.CreateContext();
+
+        await context.Database.BeginTransactionAsync();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        await service.DeleteWorkoutAsync(Constants.WORKOUT_ID);
+
+        context.ChangeTracker.Clear();
+
+        Assert.Empty(context.Workouts.ToList());
+    }
+
+    [Fact]
+    public async void DeleteWorkout_NonExistentId()
+    {
+        await using var context = Fixture.CreateContext();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        // Controller is supposed to check for the existence of the workout
+        await Assert.ThrowsAsync<DbUpdateConcurrencyException>(async () => await service.DeleteWorkoutAsync(999));
+    }
+
+    [Fact]
+    public async void AddOrUpdateWorkout_AddEmptyWorkout()
+    {
+        await using var context = Fixture.CreateContext();
+
+        await context.Database.BeginTransactionAsync();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var user = await context.Users.SingleAsync(q => q.Id == Constants.USER_ID);
+
+        var workout = new Workout
+        {
+            Date = new DateOnly(2023, 2, 5),
+            StartDate = null,
+            EndDate = null,
+            User = user,
+            Type = WorkoutTypeCode.Deadlift,
+            UserId = Constants.USER_ID
+        };
+
+        await service.AddOrUpdateWorkoutAsync(workout);
+
+        context.ChangeTracker.Clear();
+
+        Assert.NotNull(await context.Workouts.SingleOrDefaultAsync(q => q.Date == new DateOnly(2023, 2, 5)));
+    }
+
+    [Fact]
+    public async void AddOrUpdateWorkout_AddWorkoutWithEmptyExercise()
+    {
+        await using var context = Fixture.CreateContext();
+
+        await context.Database.BeginTransactionAsync();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var user = await context.Users.SingleAsync(q => q.Id == Constants.USER_ID);
+
+        var exercise = new Exercise
+        {
+            Id = 0,
+            ExerciseInfoId = 1
+        };
+
+        var workout = new Workout
+        {
+            Date = null,
+            StartDate = null,
+            EndDate = null,
+            User = user,
+            Type = WorkoutTypeCode.Bench,
+            UserId = Constants.USER_ID
+        };
+
+        workout.Exercises.Add(exercise);
+
+        await service.AddOrUpdateWorkoutAsync(workout);
+
+        context.ChangeTracker.Clear();
+
+        var addedWorkout = await context.Workouts.Include(q => q.Exercises)
+            .SingleAsync(q => q.Type == WorkoutTypeCode.Bench);
+
+        Assert.Single(addedWorkout.Exercises);
+    }
+
+    [Fact]
+    public async void AddOrUpdateWorkout_AddWorkoutWithExerciseWithOneSet()
+    {
+        await using var context = Fixture.CreateContext();
+
+        await context.Database.BeginTransactionAsync();
+
+        var service = new WorkoutsService(context, NullLogger<WorkoutsService>.Instance);
+
+        var user = await context.Users.SingleAsync(q => q.Id == Constants.USER_ID);
+
+        var exercise = new Exercise
+        {
+            Id = 0,
+            ExerciseInfoId = 1
+        };
+
+        var set = new Set
+        {
+            Id = 0,
+            Reps = 5,
+            Weight = 100,
+            Completed = false
+        };
+
+        exercise.Sets.Add(set);
+
+        var workout = new Workout
+        {
+            Date = null,
+            StartDate = null,
+            EndDate = null,
+            User = user,
+            Type = WorkoutTypeCode.Bench,
+            UserId = Constants.USER_ID
+        };
+
+        workout.Exercises.Add(exercise);
+
+        await service.AddOrUpdateWorkoutAsync(workout);
+
+        context.ChangeTracker.Clear();
+
+        var addedWorkout = await context.Workouts.Include(q => q.Exercises)
+            .ThenInclude(q => q.Sets)
+            .SingleAsync(q => q.Type == WorkoutTypeCode.Bench);
+
+        Assert.Equal(5, addedWorkout.Exercises.First().Sets.First().Reps);
+    }
+}
